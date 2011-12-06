7a2226633fd96a4cc81ab8f6607999a5f5e43632
Vincent Pretre <v.pretre@zestsoftware.nl>
2011-12-06 10:36:07 +0100

    Added buildout creation + running the 'bin/buildout' command

--- zest/recipe/mk_buildout/mk_buildout.py
+++ zest/recipe/mk_buildout/mk_buildout.py
@@ -7,11 +7,13 @@ class MakeBuildout(object):
     _allowed_options = ['recipe',
                         'python', 'paster',
                         'template', 'buildout_file',
+                        'buildout_rename',
                         'paster_commands']
     _base_options = {'python': 'python',
                      'paster': 'paster',
                      'template': 'plone',
                      'buildout_file': 'buildout.cfg',
+                     'buildout_rename': 'buildout_base.cfg',
                      'paster_commands': ''}
 
     def __init__(self, buildout, name, options):
@@ -21,13 +23,16 @@ class MakeBuildout(object):
         self.options.update(self._base_options)
         self.options.update(options)
 
-        #install_dir = self.buildout['buildout']['directory']
-
     @property
     def logger(self):
         return logging.getLogger(self.name)
 
     def _check_command_line(self, command, expected):
+        """ Checks that 'excepted' is in the output of the command.
+        Command must be a list of string (['ls', '-la'] for example for 'ls -la')
+        Raises a ValueError if the command is not found or the excepted
+        string is not found in the output.
+        """
         try:
             output,error = subprocess.Popen(command, stdout = subprocess.PIPE, stderr= subprocess.PIPE).communicate()
         except OSError:
@@ -41,9 +46,14 @@ class MakeBuildout(object):
             raise ValueError
 
     def check_options(self):
+        """ Checks that options provided are correct:
+        - python must be a python executable
+        - same for paster
+        - template must be available in paster templates
+        """
         for option, value in self.options.items():
             if option not in self._allowed_options:
-                self.logger.info('option %s is not recognized' % option)
+                self.logger.warn('option %s is not recognized' % option)
                 continue
         
             if option == 'python':
@@ -63,11 +73,19 @@ class MakeBuildout(object):
                         '  %s:' % value)
                 except ValueError:
                     self.logger.error('Template "%s" unknown in paster' % value)
-                    raise ValueError
+                    raise ValueError       
 
+    def go_to_parts(self):
+        os.chdir(self.buildout['buildout']['parts-directory'])
 
-    def install(self):
-        self.check_options()
+    def go_to_subbuildout(self):
+        os.chdir(os.sep.join([self.buildout['buildout']['parts-directory'],
+                              self.name]))
+
+    def create_buildout(self):
+        """ Runs the paster command to create the buildout.
+        """
+        self.go_to_parts()
         self.logger.info('Creating new sub buildout')
         self.logger.info('Running %s create -t %s %s' % (
             self.options['paster'],
@@ -77,15 +95,57 @@ class MakeBuildout(object):
         paster_input = os.tmpfile()
         paster_input.write(self.options['paster_commands'].replace("''", ""))
         paster_input.seek(0)
-        subprocess.Popen([self.options['paster'],
-                          'create',
-                          '-t',
-                          self.options['template'],
-                          self.name],
-                         stdin = paster_input)
+        p = subprocess.Popen([self.options['paster'],
+                              'create',
+                              '-t',
+                              self.options['template'],
+                              self.name],
+                             stdin = paster_input)
+        p.wait()
         paster_input.close()
+
+    def add_buildout_file(self):
+        """ If needed, will change buildout.cfg in buildout_base.cfg.
+        Then creates a buildout.cfg extending the 'buildout_file' option (
+        or buildout_base.cfg if it was changed previously).
+        """
+        self.go_to_subbuildout()
+        extends = self.options['buildout_file']
+
+        if self.options['buildout_file'] == 'buildout.cfg':
+            if not os.path.exists(self.options['buildout_rename']):
+                os.rename('buildout.cfg', self.options['buildout_rename'])
+            extends = self.options['buildout_rename']
+
+        b = open('buildout.cfg', 'w')
+        b.write("[buildout]\n")
+        b.write("extends = %s\n" % extends)
+        b.close()
+
+    def run_bootstrap(self):
+        """ If bin/buildout does not exist, runs 'python bootstrap.py'
+        """
+        if os.path.exists(os.sep.join(['bin', 'buildout'])):
+            return
+
+        p = subprocess.Popen([self.options['python'], 'bootstrap.py'])
+        p.wait()
+
+    def run_buildout(self):
+        """ Run the 'bin/buildout command'
+        """
+        self.go_to_subbuildout()        
+        self.run_bootstrap()
+        p = subprocess.Popen(['bin/buildout'])
+        p.wait()
+
+    def install(self):
+        self.check_options()
+        self.create_buildout()
+        self.add_buildout_file()
+        self.run_buildout()
         return []
 
     def update(self):
         self.check_options()
-
+        self.run_buildout()
